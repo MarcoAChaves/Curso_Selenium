dd011c73b3ee64cd1815d11078163817c56a2569
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-25 18:16:09 -0300

    Implementando Junit

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,13 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 
-    public static void main(String[] args) {
+    @Test
+    public void teste() {
 
         //System.setProperty("webdriver.gecko.driver", "C:/geckodriver/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("https://google.com");
-        System.out.println(driver.getTitle());
+        Assert.assertEquals("Google", driver.getTitle());
     }
 }
