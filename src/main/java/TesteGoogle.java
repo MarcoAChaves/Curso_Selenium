e99c20d0c60b3ef42409a36417c0d60c9608939b
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-25 19:12:09 -0300

    Posição e tamanho do browser

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,5 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -18,6 +20,8 @@ public class TesteGoogle {
         WebDriver driver = new FirefoxDriver();
         //WebDriver driver = new ChromeDriver();
         //WebDriver driver = new EdgeDriver();
+        driver.manage().window().setPosition(new Point(200, 200));
+        driver.manage().window().setSize(new Dimension(1200, 765));
         driver.get("https://google.com");
         System.out.println(driver.getTitle());
         Assert.assertEquals("Google", driver.getTitle());
