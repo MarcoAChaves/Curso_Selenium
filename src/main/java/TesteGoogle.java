95f293b7cde3bf6927b0886c061cedbeaa554e31
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-25 16:57:50 -0300

    Atualizando com a versão antiga do firefox

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -5,9 +5,9 @@ public class TesteGoogle {
 
     public static void main(String[] args) {
 
-        //System.setProperty("webdriver.gecko.driver", "C:\\WorkSpace\\Programas\\geckodriver-v0.34.0-win-aarch64.exe");
+        //System.setProperty("webdriver.gecko.driver", "C:/geckodriver/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
-        driver.get("http://google.com");
+        driver.get("https://google.com");
         System.out.println(driver.getTitle());
     }
 }
