10e4e59c4972dbafa337b834abada59d294fae75
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-25 18:56:51 -0300

    Implementando outros navegadorres

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,16 +1,25 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TesteGoogle {
 
     @Test
     public void teste() {
 
+        //System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+        //System.setProperty("webdriver.Chrome.driver", "C:\\Chromedriver\\chromedriver-win64.exe");
         //System.setProperty("webdriver.gecko.driver", "C:/geckodriver/geckodriver.exe");
+
         WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new EdgeDriver();
         driver.get("https://google.com");
+        System.out.println(driver.getTitle());
         Assert.assertEquals("Google", driver.getTitle());
     }
 }
