c573a08f28fbac4c3f8ae5172c171b1e011086c7
Cordeiro Silva Chaves <mocv@gft.com>
2024-01-26 10:14:32 -0300

    Elementos básicos: Combo

--- src/main/java/marco/chaves/TesteCampoTreinamento.java
+++ src/main/java/marco/chaves/TesteCampoTreinamento.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class TesteCampoTreinamento {
 
     @Test
-    public void testeTextField(){
+    public void testeTextField() {
 
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
@@ -18,28 +22,26 @@ public class TesteCampoTreinamento {
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
         Assert.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-
         driver.quit();
     }
+
     @Test
-    public void deveInteragirComTextArea(){
+    public void deveInteragirComTextArea() {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste");
         Assert.assertEquals("teste", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
-
         driver.quit();
     }
 
     @Test
-    public void deveInteragirComRadioButton(){
+    public void deveInteragirComRadioButton() {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:sexo:0")).click();
         Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
-
         driver.quit();
     }
 
@@ -50,7 +52,42 @@ public class TesteCampoTreinamento {
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
         Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+        driver.quit();
+    }
 
+    @Test
+    public void deveInteragirComCombo() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(element);
+        //combo.selectByIndex(2); - modo de busca por posição
+        //combo.selectByValue("superior"); - modo de busca por valor
+        combo.selectByVisibleText("2o grau completo"); //modo de busca por valor visível
+
+        Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+        driver.quit();
+    }
+
+    @Test
+    public void deveVerificarValoresCombo() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(element);
+        List<WebElement> options = combo.getOptions();
+        Assert.assertEquals(8, options.size());
+
+        boolean encontrou = false;
+        for (WebElement option: options){
+            if (option.getText().equals("Mestrado")){
+                encontrou = true;
+                break;
+            }
+        }
+        Assert.assertTrue(encontrou);
         driver.quit();
     }
 }
