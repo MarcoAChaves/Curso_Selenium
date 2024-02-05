dfd9bd9e000918528491ac6e4d53b9367afb5f8e
Cordeiro Silva Chaves <mocv@gft.com>
2024-02-05 13:19:27 -0300

    Criando uma Suíte de Testes

---
+++ src/main/java/marco/chaves/SuiteTeste.java
@@ -0,0 +1,13 @@
+package marco.chaves;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DesafioAula.class,
+        TesteRegrasCadastro.class,
+        TesteRegraDeNegocio.class
+})
+public class SuiteTeste {
+}
