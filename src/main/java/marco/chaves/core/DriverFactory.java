35813f8fcd7044c3090fc1fc1c11da8d6b1abc7b
Cordeiro Silva Chaves <mocv@gft.com>
2024-02-09 19:20:21 -0300

    Chaveamento de browser

--- src/main/java/marco/chaves/core/DriverFactory.java
+++ src/main/java/marco/chaves/core/DriverFactory.java
@@ -2,7 +2,11 @@ package marco.chaves.core;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.openqa.selenium.remote.Browser.CHROME;
+import static org.openqa.selenium.remote.Browser.FIREFOX;
 
 public class DriverFactory {
 
@@ -13,7 +17,10 @@ public class DriverFactory {
 
     public static WebDriver getDriver(){
         if (driver ==null){
-            driver = new FirefoxDriver();
+            switch (Propriedades.browser){
+                case FIREFOX: driver = new FirefoxDriver(); break;
+                case CHROME: driver = new ChromeDriver(); break;
+            }
             driver.manage().window().setSize(new Dimension(1200, 765));
         }
         return driver;
--- src/main/java/marco/chaves/core/Propriedades.java
+++ src/main/java/marco/chaves/core/Propriedades.java
@@ -5,4 +5,11 @@ public class Propriedades {
     public static boolean FECHAR_BROWSER = false;
     // true - abre e fecha o browser a cada execução, almentando o tempo de teste.
     // false - abre o browser apenas uma vez, otimizando o tempo de execução
+
+    public static Browsers browser = Browsers.CHROME;
+
+    public enum Browsers {
+        CHROME,
+        FIREFOX
+    }
 }
