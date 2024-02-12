420169ebfb2d6f13560343afd78e9ad41f084d51
Cordeiro Silva Chaves <mocv@gft.com>
2024-02-12 12:22:35 -0300

    Removendo commit Sr barriga

--- src/main/java/marco/chaves/core/DriverFactory.java
+++ src/main/java/marco/chaves/core/DriverFactory.java
@@ -12,25 +12,21 @@ public class DriverFactory {
 
     private static WebDriver driver;
 
-    private DriverFactory() {
+    private DriverFactory(){
     }
 
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            switch (Propriedades.browser) {
-                case FIREFOX:
-                    driver = new FirefoxDriver();
-                    break;
-                case CHROME:
-                    driver = new ChromeDriver();
-                    break;
+    public static WebDriver getDriver(){
+        if (driver ==null){
+            switch (Propriedades.browser){
+                case FIREFOX: driver = new FirefoxDriver(); break;
+                case CHROME: driver = new ChromeDriver(); break;
             }
             driver.manage().window().setSize(new Dimension(1200, 765));
         }
         return driver;
     }
 
-    public static void killDriver() {
+    public static void killDriver(){
         if (driver != null) {
             driver.quit();
             driver = null;
--- src/main/java/marco/chaves/test/TesteGoogle.java
+++ src/main/java/marco/chaves/test/TesteGoogle.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TesteGoogle {
 
@@ -17,7 +20,7 @@ public class TesteGoogle {
         System.setProperty("webdriver.gecko.driver", "C:/geckodriver/geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
-        // WebDriver driver = new ChromeDriver();
+       // WebDriver driver = new ChromeDriver();
         //WebDriver driver = new EdgeDriver();
         driver.manage().window().setPosition(new Point(100, 100));
         driver.manage().window().setSize(new Dimension(1200, 765));
